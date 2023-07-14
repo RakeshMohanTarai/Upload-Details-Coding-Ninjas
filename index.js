d55ec69f97a19dc9069fb4470d5413f8351786ce
Rakesh Mohan Tarai <138197175+RakeshMohanTarai@users.noreply.github.com>
2023-07-14 14:52:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function allowOnlyAlphabets(event) {
-      var key = event.key;
-      if (/^[A-Za-z]$/.test(key)) {
-        return true;
-      } else {
-        event.preventDefault();
-      }
+  function allowOnlyAlphabets(event) {
+    var key = event.key;
+    if (!/^[A-Za-z ]$/.test(key)) {
+      event.preventDefault();
+    }
+  }
+
+  function allowOnlyNumbers(event) {
+    var key = event.key;
+    if (!/^\d$/.test(key)) {
+      event.preventDefault();
+    }
+  }
+
+  document.getElementById('studentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission from refreshing the page
+
+    // Retrieve form inputs
+    var name = document.getElementById('nameInput').value;
+    var rollNumber = document.getElementById('rollNumberInput').value;
+    var marks = document.getElementById('marksInput').value;
+
+    // Validate marks value
+    if (marks !== '' && isNaN(marks)) {
+      alert("Marks value must be numeric");
+      return;
     }
 
-    function allowOnlyNumbers(event) {
-      var key = event.key;
-      if (/^\d$/.test(key)) {
-        return true;
-      } else {
-        event.preventDefault();
-      }
+    // Validate roll number value
+    if (rollNumber !== '' && isNaN(rollNumber)) {
+      alert("Roll number must be numeric");
+      return;
     }
 
-    document.getElementById('studentForm').addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent form submission from refreshing the page
+    // Create a new student record
+    var studentRecord = document.createElement('div');
+    studentRecord.classList.add('student-record');
+    studentRecord.style.backgroundColor = 'green';
+    studentRecord.innerHTML = 'Roll No - ' + rollNumber + ', ' + name + ' has scored ' + marks + ' marks';
 
-      // Retrieve form inputs
-      var name = document.getElementById('nameInput').value;
-      var rollNumber = document.getElementById('rollNumberInput').value;
-      var marks = parseInt(document.getElementById('marksInput').value, 10);
+    // Append the new record to the student list
+    document.getElementById('studentList').appendChild(studentRecord);
 
-      // Validate marks value
-      if (marks > 100) {
-        alert("Marks value cannot be greater than 100");
-        return;
-      }
-
-      // Create a new student record
-      var studentRecord = document.createElement('div');
-      studentRecord.classList.add('student-record');
-      studentRecord.style.backgroundColor = 'green';
-      studentRecord.innerHTML = 'Roll No - ' + rollNumber + ', ' + name + ' has scored ' + marks + ' marks';
-
-      // Append the new record to the student list
-      document.getElementById('studentList').appendChild(studentRecord);
-
-      // Reset the form inputs
-      document.getElementById('nameInput').value = '';
-      document.getElementById('rollNumberInput').value = '';
-      document.getElementById('marksInput').value = '';
-    });
-
-    document.getElementById('nameInput').addEventListener('keypress', allowOnlyAlphabets);
-    document.getElementById('rollNumberInput').addEventListener('keypress', allowOnlyNumbers);
+    // Reset the form inputs
+    document.getElementById('nameInput').value = '';
+    document.getElementById('rollNumberInput').value = '';
+    document.getElementById('marksInput').value = '';
   });
+
+  document.getElementById('nameInput').addEventListener('keypress', allowOnlyAlphabets);
+  document.getElementById('rollNumberInput').addEventListener('keypress', allowOnlyNumbers);
+  document.getElementById('marksInput').addEventListener('keypress', allowOnlyNumbers);
+});
+  
+  // Add event listener to the form submission
+  document.getElementById("studentForm").addEventListener("submit", function(event) {
+    // Your desired code for form submission
+  });
+
+  // Restrict keyboard input for marksInput field
+  document.getElementById("marksInput").addEventListener("input", function(event) {
+    var marksInput = event.target;
+    var marks = parseInt(marksInput.value);
+    
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+      marksInput.value = ""; // Clear the input if it is not a valid number or out of range
+    }
+  });   
